6130462f225c8fb4df89c661b62b49bebecfe509
Cory Grube <cory.grube@ThermoSystems.local>
2020-05-06 22:56:51 -0400

    main loop restructured additional variables/instances added steamModeInp() created/finalized

--- src/main.cpp
+++ src/main.cpp
@@ -13,39 +13,51 @@ int pin_CLK = 13;           // MAX31865 SPI CLK pin
 
 
 /* Global Variables */
-// Inputs
-float g_BoilerTemp;   // Boiler water temperature (DegF)
+double g_BoilerTemp;                     // Boiler water temperature (DegF)
+double g_BoilerCmd;                      // Boiler heater SSR PWM command (0-255)
+double g_BoilerSp;                       // Boiler water temperature setpoint (DegF)
+bool g_BoilerTempFault;                  // Boiler water temperature fault (1=Fault)
+bool g_SteamMode;                        // Machine Steam Mode (0=espresso mode, 1=steam mode)
+int g_SteamModeRawPrev;                  // value of Steam Mode pin on previous scan (1=steam mode)
+unsigned long g_SteamModeDbPrev;         // Previous time Steam Mode pin was toggled (ms)
+unsigned long g_SteamModeDbCfg = 50;     // Configured debounce time for toggle (ms)
 
-// Outputs
-float g_BoilerCmd;    // Boiler heater SSR command (0-100%)
+// Adafruit MAX31865 instance (RTD sensor board) (CS, SDI, SDO, CLK)
+Adafruit_MAX31865 g_BoilerRtd = Adafruit_MAX31865(pin_CS, pin_SDI, pin_SDO, pin_CLK);     // Boiler RTD MAX31865
 
-// Adafruit MAX31865 (RTD sensor board)
-Adafruit_MAX31865 g_BoilerRtd = Adafruit_MAX31865(pin_CS, pin_SDI, pin_SDO, pin_CLK);     // Boiler RTD MAX31865 instance
-
+// PID Instance (Input, Output, Setpoint, kP, kI, kD, POn Direction)
+PID g_BoilerPid = PID(&g_BoilerTemp, &g_BoilerCmd, &g_BoilerSp, 1, 1, 0, P_ON_M, DIRECT); // Boiler Temp PID
 
 /* Function Declarations */
-void boilerTempInp();
-void boilerTempPID();
+void boilerTempInp(), steamModeInp(), boilerTempControl();
 
 
 /* Setup */
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   g_BoilerRtd.begin(MAX31865_3WIRE);  //RTD initialization
+  
+  // Pin configurations
+  pinMode(pin_SteamMode, INPUT);
+  pinMode(pin_BoilerCmd, OUTPUT);
 }
 
 
 /* Main Loop */
 void loop() {
-  /* Inputs/Reads */
+  // Inputs/Reads
   boilerTempInp();
+  steamModeInp();
+  
 
-  /* Processing/Calculations */
-  // boilerTempPid();
+  // Processing/Calculations
+  boilerTempControl();
 
-  /* Outputs/Writes */
-  int boilerCmdRaw = g_BoilerCmd / 100 * 255;   // Scaling from 0-100% to 0-255 for PWM
-  analogWrite(pin_BoilerCmd, pin_BoilerCmd);
+  // Monitoring
+  int boilerCmdPct = g_BoilerCmd / 255 * 100;   // Scaling from PWM 0-255 to 0-100% for Diagnostics
+
+  // Outputs/Writes
+  analogWrite(pin_BoilerCmd, g_BoilerCmd);
 }
 
 
@@ -59,6 +71,7 @@ void boilerTempInp() {
   //Boiler temperature fault check
   byte fault = g_BoilerRtd.readFault();                                   // Check MAX board for fault codes
   if (fault) {                                                            // If fault is present, print fault code and description of fault
+    g_BoilerTempFault = 1;                                                // Set global temp fault if one exists
     Serial.print("RTD Fault 0x"); Serial.println(fault, HEX);
     if (fault & MAX31865_FAULT_HIGHTHRESH) {
       Serial.println("RTD High Threshold");
@@ -80,10 +93,44 @@ void boilerTempInp() {
     }
     g_BoilerRtd.clearFault();
   }
+  else g_BoilerTempFault = 0;                                             // Clear global temp fault if none exists
   Serial.println();
 }
 
 
-void boilerTempPid() {
+void steamModeInp() {
+// read the state of the switch into a local variable:
+  int steamModeRaw = digitalRead(pin_SteamMode);
+  
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
 
+  // If the switch changed, due to noise or pressing:
+  if (steamModeRaw != g_SteamModeRawPrev) {
+    // reset the debouncing timer
+    g_SteamModeDbPrev = millis();
+  }
+
+    if ((millis() - g_SteamModeDbPrev) > g_SteamModeDbCfg) {
+    // whatever the steamModeRaw is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the mode has changed:
+      if (steamModeRaw != g_SteamMode) {
+        // set the debounced steam mode to the raw value
+        g_SteamMode = steamModeRaw;
+      }
+  }
+
+    // save the steamModeRaw. Next time through the loop, it'll be the g_SteamModeRawPrev:
+  g_SteamModeRawPrev = steamModeRaw;
+
+}
+
+void boilerTempControl() {
+// if (fdsa) 
+
+
+g_BoilerPid.Compute();
 }
